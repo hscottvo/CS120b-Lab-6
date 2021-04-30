a225ae45831bd9eb7eb79a3b081d3e36b915d71e
hscottvo <hscottvo@gmail.com>
2021-04-29 19:40:11 -0700

    Added timer test files

--- source/main.c
+++ source/main.c
@@ -8,16 +8,24 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "timer.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    TimerSet(1000);
+    TimerOn();
+    unsigned char tmpB = 0x00;
     /* Insert your solution below */
     while (1) {
-
+        tmpB = ~tmpB;
+        PORTB = tmpB;
+        while (TimerFlag);
+        TimerFlag = 0;
     }
     return 1;
 }
